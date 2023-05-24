a72fbc7a2b1141f6677ed820f7051020de420406
Jean Gonzales <expretapador3@gmail.com>
2023-05-23 22:23:39 -0500

    agregando interfaz showMenu y aplicando variables static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,10 @@
 public class Doctor {
     //atributos
-    int id;
+    static int id = 0; //Autoincrement
     String name, speciality;
     Doctor () {
         System.out.println("calling constructor");
+        id ++;
     }
     Doctor (String name) {
         System.out.println("el nombre del doc es: " + name);
@@ -11,5 +12,8 @@ public class Doctor {
     //comportamientos
     public void showName () {
         System.out.println(name);
+    }
+    public void showId () {
+        System.out.println("ID del doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Doc Perez");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Jean Gonzales";
         myDoctor.showName();
+        System.out.println("Id del doctor: " + Doctor.id); //una forma
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId(); // la otra forma, esta es mas adecuada
+
+        showMenu();
+
     }
 }
