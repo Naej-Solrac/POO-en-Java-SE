20ac021db0e63702141e75bb1bbcbb2b74d4d1c7
Jean Gonzales <expretapador3@gmail.com>
2023-05-24 19:00:46 -0500

    utilizando metodos y variables estaticas para tener mas alcance creando package uiMenu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("");
         myDoctor.name = "Jean Gonzales";
         myDoctor.showName();
         System.out.println("Id del doctor: " + Doctor.id); //una forma
