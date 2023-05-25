6f67dfd9d37bd5b37371df3d0e94a82fb9c028e4
Jean Gonzales <expretapador3@gmail.com>
2023-05-25 11:24:45 -0500

    reto de hacer clase Patient

---
+++ src/Patient.java
@@ -0,0 +1,8 @@
+public class Patient {
+    String name, email, address, phoneNumber, birthday, blood;
+    double weight, height;
+    Patient (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
