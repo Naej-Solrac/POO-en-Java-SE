20fe6324e4d7bf855124963b81baf23beb6fe778
Jean Gonzales <expretapador3@gmail.com>
2023-05-24 19:05:32 -0500

    Limpieza dela clase main para continuar con el programa

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("");
-        myDoctor.name = "Jean Gonzales";
-        myDoctor.showName();
-        System.out.println("Id del doctor: " + Doctor.id); //una forma
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctorAnn.showId(); // la otra forma, esta es mas adecuada
-
         showMenu();
 
     }
