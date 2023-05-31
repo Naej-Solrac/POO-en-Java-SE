9d41e892488f3a1a99426dd161d333715954debd
Jean Gonzales <expretapador3@gmail.com>
2023-05-30 22:19:31 -0500

    se crea User para usar herencia en Doctor y Patient, uso del extends y super

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,24 +1,24 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
-    //atributos
-    static int id = 0; //Autoincrement
-    private String name; private String speciality; private String email;
+public class Doctor extends User{ //herencia
+    private String speciality;
 
-    Doctor (String name, String speciality) {
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    Doctor (String name, String email) {
+        super(name, email); //herencia
         System.out.println("el nombre del doc es: " + name);
-        id ++;
-        this.name = name;
+
         this.speciality = speciality;
     }
     //comportamientos
-    public void showName () {
-        System.out.println(name);
-    }
-    public void showId () {
-        System.out.println("ID del doctor: " + id);
-    }
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>(); //coleccion
     public void addAvailableAppointment (Date date, String time) {
@@ -63,5 +63,7 @@ public class Doctor {
         public void setTime(String time) {
             this.time = time;
         }
+
+
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,13 +1,10 @@
-public class Patient {
+public class Patient extends User{ //herencia
     //atributos
-    int id;
-    private String name;
-    private String email, address, phoneNumber, birthday, blood;
+    private String  birthday, blood;
     private double weight, height;
 
     Patient(String name, String email) {
-        this.name = name;
-        this.email = email;
+        super(name, email); //herencia
     }
 
     //54.5
@@ -28,48 +25,9 @@ public class Patient {
         this.height = height;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 
     public void setPhoneNumber(String phoneNumber) {
-        if (phoneNumber.length() > 9) {
-            System.out.println("el num debe ser 9 digitos como max");
-        } else if (phoneNumber.length() == 8) {
-            this.phoneNumber = phoneNumber;
-        }
+
     }
 
     public String getBirthday() {
