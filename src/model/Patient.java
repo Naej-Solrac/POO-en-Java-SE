f1b0e3e7b757a4fc371640f2507438e8b7f64edd
Jean Gonzales <expretapador3@gmail.com>
2023-06-13 17:13:39 -0500

    Agendando citas

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -2,10 +2,34 @@ package model;
 
 import model.User;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Patient extends User { //herencia
     //atributos
     private String  birthday, blood;
     private double weight, height;
+
+    private ArrayList<AppointmentDoctor> appointmentDoctors = new ArrayList<>();
+    private ArrayList<AppointmentNurse> appointmentNurses = new ArrayList<>();
+
+    public ArrayList<AppointmentDoctor> getAppointmentDoctors() {
+        return appointmentDoctors;
+    }
+
+    public void addAppointmentDoctors(Doctor doctor, Date date, String time) {
+        AppointmentDoctor appointmentDoctor = new AppointmentDoctor(this, doctor);
+        appointmentDoctor.schedulable(date,time);
+        appointmentDoctors.add(appointmentDoctor);
+    }
+
+    public ArrayList<AppointmentNurse> getAppointmentNurses() {
+        return appointmentNurses;
+    }
+
+    public void setAppointmentNurses(ArrayList<AppointmentNurse> appointmentNurses) {
+        this.appointmentNurses = appointmentNurses;
+    }
 
     public Patient(String name, String email) {
         super(name, email); //herencia
